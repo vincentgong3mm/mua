12e06d7a745365071c07cdfbef3601df5e903e96
공 성배 <vincentgong3mm@gmail.com>
2016-03-11 00:46:00 +0900

    send to client echo message

--- src/tcp_accept.erl
+++ src/tcp_accept.erl
@@ -3,18 +3,18 @@
 
 -define (ACCEPT_TIMEOUT, 250).
 
--record(state, {parent, name, port, listenSock, log_accept_count = 0}).
+-record(state, {parent, name, handler, port, listen_sock, log_accept_count = 0}).
 
 -export([
-    start_link/2
+    start_link/3
     ]).
 -export([
     init/2, 
     loop/1
     ]).
 
-start_link(Name, Port) ->
-    State = #state{name=Name, port=Port},
+start_link(Name, Handler, Port) ->
+    State = #state{name = Name, handler = Handler, port = Port},
     proc_lib:start_link(?MODULE, init, [self(), State]).
     
 init(Parent, State) ->
@@ -22,20 +22,22 @@ init(Parent, State) ->
     {ok, ListenSock} = gen_tcp:listen(Port, 
                                 [binary, {packet, 0}, {active, false}, {reuseaddr, true}]),
     
-    State2 = State#state{parent=Parent, listenSock=ListenSock},
+    State2 = State#state{parent=Parent, listen_sock=ListenSock},
     proc_lib:init_ack(Parent, {ok, self()}),
     loop(State2).
     
  loop(State) ->
     ?LOG(State),
-    case gen_tcp:accept(State#state.listenSock) of
+    case gen_tcp:accept(State#state.listen_sock) of
     {ok, ClientSock} ->
-        Log_accept_count=State#state.log_accept_count, 
-        State2 = State#state{log_accept_count = Log_accept_count + 1},
-        
-        % spawn recv process 
         ?LOG("connect Client"),
         
+        % create receive process for one client.
+        tcp_receive:start_link(State#state.name, State#state.handler, ClientSock),
+        
+        Log_accept_count = State#state.log_accept_count, 
+        State2 = State#state{log_accept_count = Log_accept_count + 1},
+                
         % loop
         ?LOG(State2),
         
--- src/tcp_receive.erl
+++ src/tcp_receive.erl
@@ -1,7 +1,14 @@
 -module(tcp_receive).
 -include("mua_const.hrl").
 
--record(state, {parent, name, client_sock, handler, log_receive_count = 0}).
+%% receve 프로세스에서 참조할 값들
+-record(state, {
+    parent, % parent process id 
+    name, % 로그용 프로세스이름
+    handler, % 패킷받았을 때 호출할 Moudle
+    client_sock,  % client socket
+    log_receive_count = 0   % 로그용. 패킷받은 횟수
+    }).
 
 -export([
     start_link/3
@@ -11,46 +18,68 @@
     loop/1
     ]).
 
-start_link(Name, ClientSock, Handler) ->
+start_link(Name, Handler, ClientSock) ->
     State = #state{name=Name, client_sock = ClientSock, handler = Handler},
+    
+    % 프로레스 생성
     proc_lib:start_link(?MODULE, init, [self(), State]).
 
 init(Parent, State) ->
+    State2 = State#state{parent=Parent},
+    
+    % proc_lib:start_link를 생성하면 반드시 init_ack해줘야함.
     proc_lib:init_ack(Parent, {ok, self()}),
-    loop(State).
+    
+    % init_ack하고 loop
+    loop(State),
+    
+    % loop가 끝나면 프로레스 종료하는 것임.
+    ?LOG("terminate process").
     
 loop(State) ->
+    ?LOG(State),
     ClientSock = State#state.client_sock,
     Handler = State#state.handler,
     case do_recv(ClientSock) of
         {ok, BinRecv} ->
-            Handler:recv_packet({ok, BinRecv}),
-        
-            State2 = State#state.log_receive_count + 1,
-            loop(State2);
-
-        {error, close} ->
-            Handler:recv_packet({error, close}),
+            % call handler function
+            ?LOG(BinRecv),
+            %Handler:recv_packet({ok, BinRecv}),
+           
+            % 그냥 로그를 위해서 패킷받은 횟수 저장
+            Log_receive_count = State#state.log_receive_count, 
+            State2 = State#state{log_receive_count = Log_receive_count + 1},
             
-            % maybe termicate process
+            % 다시 recv하기 위해서 loop
+            loop(State2);
+        
+        {error, close} ->
+            % call handler function
+            gen_tcp:close(ClientSock),
+            %Handler:recv_packet({error, close}),
             {error, close}
     end.    
         
 do_recv(ClientSock) ->
-    ?LOG("dorecv"),
     case gen_tcp:recv(ClientSock, 0) of
         {ok, BinRecv} ->
             %% \r\n제거 
             case binary:match(BinRecv, [<<"\r\n">>]) of
             {Pos, Length} ->  
                 B2 = binary:part(BinRecv, {0, Pos}),
-                ?LOG(B2),
+                
+                % echo
+                gen_tcp:send(ClientSock, <<"echo:">>),
+                gen_tcp:send(ClientSock, B2),
+                gen_tcp:send(ClientSock, <<"\r\n">>),
+                
                 {ok, B2};
             nomatch ->
-                ?LOG("nomatch ---------"),
+                % 테스트로 \r\n이 없는 경우(console에서 ctrl+C)는 서버가 끊어졌다고 인정.
                 {error, close}  
             end;
         {error, close} ->
+            ?LOG("disconnect??"),
             {ok, list_to_binary([])}
       end.
     

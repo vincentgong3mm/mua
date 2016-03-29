a7a9214f65a80ad6d421a443fa25e9fc3409eb25
공 성배 <vincentgong3mm@gmail.com>
2016-03-30 08:09:41 +0900

    test supervisor, start_child

--- src/mua_sup.erl
+++ src/mua_sup.erl
@@ -1,11 +1,13 @@
 -module(mua_sup).
-
+-include("mua_const.hrl").  % for ?LOG
 -behaviour(supervisor).
 
 %% API
 -export([
     start_link/0, 
+    start_child/1,
     start_child/2,
+    start_child/3,
     start_all_child/0
     ]).
 
@@ -28,12 +30,16 @@ start_all_child() ->
     start_child(accept, {server1, 8088}),
     start_child(accept, {server2, 8089}),
     tcp_accept:accept().
+
+
+start_child(test) ->
+    ?LOG("start child test"),
+    supervisor:start_child(?MODULE, []).
     
+% test start child tcp_accept module
 start_child(accept, {Name, Port}) ->
     io:format("mua_sup start_chind accept~n"),
-   
-    supervisor:start_child(?MODULE, [Name, Port]);        
-
+    supervisor:start_child(?MODULE, [Name, temp_handle, Port]);
 start_child(recv_packet, {ClientSock, Handler}) ->
     io:format("mua_sup start_chind receive~n"),
     
@@ -41,6 +47,10 @@ start_child(recv_packet, {ClientSock, Handler}) ->
                   permanent, 2000, worker, [tcp_receive]},
 
     supervisor:start_child(?MODULE, Receive).
+
+start_child(listener, {Name, Module, Pid}, Port) ->
+    ?LOG("start child listener"),
+    supervisor:start_child(?MODULE, [Name, {temp_handle, temp_pid}, Port]).
 
 
 %% ===================================================================
@@ -58,10 +68,19 @@ init([]) ->
     Accept = {tcp_accept, {tcp_accept, start_link, []},
                   permanent, 2000, worker, [tcp_accept]},
 
+    AsyncListner = {tcp_async_listener, {tcp_async_listener, start_link, []},
+                  permanent, 2000, worker, [tcp_async_listener]},
+
+    TestHandler = {tcp_test_async_handler, {tcp_test_async_handler, start_link, []},
+                  permanent, 2000, worker, [tcp_test_async_handler]},
+
+
     %%Receive = {tcp_receive, {tcp_receive, start_link, [1000, conn_man]},
     %%              permanent, 2000, worker, [tcp_receive]},
     
-    {ok, {SupFlags, [Accept]}}.              
+    {ok, {SupFlags, [TestHandler]}}.
+    %{ok, {SupFlags, [AsyncListner]}}.
+    %{ok, {SupFlags, [Accept, AsyncListner]}}.              
     %%{ok, {SupFlags, [Accept, Receive]}}.
     %%{ok, {SupFlags, [Accept, Receive]}}.
     
